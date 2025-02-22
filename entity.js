e60626bd10c36b4ebf6fe7555086f74c0d79c1a6
Yann Coleu <y@nn-col.eu>
2025-02-22 18:23:56 +0100

    steps cleaning and refactoring

--- entity.js
+++ entity.js
@@ -1,65 +1,75 @@
-const FPS60 = 16
+const FPS60 = 16;
 class Entity {
-    constructor(x, y, el) {
-        this.el = el;
-        this.x = x;
-        this.y = y;
+  constructor(x, y, el) {
+    this.el = el;
+    this.x = x;
+    this.y = y;
+    this.cancelAnimation = function () {};
 
-        this.el.style.left =  `${x}vw`;
-        this.el.style.top =  `${y}vh`;
-    }
-    moveTo(x, y, dur) {
-        const startDate = performance.now();
-        const shouldEndDate = startDate + dur;
-        const startX = this.x;
-        const startY = this.y;
-        const interval = 1;
-        const nbIter = dur / interval;
+    this.el.style.left = `${x}vw`;
+    this.el.style.top = `${y}vh`;
+  }
+  moveTo(x, y, dur) {
+    this.cancelAnimation();
+    const startDate = performance.now();
+    const shouldEndDate = startDate + dur;
+    const startX = this.x;
+    const startY = this.y;
+    const interval = 1;
+    const nbIter = dur / interval;
 
-        var intervalId = setInterval(() => {
-            const tickDate = performance.now();
-            const elapsedSince = tickDate - startDate;
+    var intervalId = setInterval(() => {
+      const tickDate = performance.now();
+      const elapsedSince = tickDate - startDate;
 
-            const moveCoef = elapsedSince / dur;
-            const distX = x - startX;
-            const distY = y - startY;
+      const moveCoef = elapsedSince / dur;
+      const distX = x - startX;
+      const distY = y - startY;
 
-            this.setX(startX + distX * moveCoef);
-            this.setY(startY + distY * moveCoef);
-        }, interval);
+      this.setX(startX + distX * moveCoef);
+      this.setY(startY + distY * moveCoef);
+    }, interval);
 
-        setTimeout(() => {
-            clearInterval(intervalId);
-            console.log("interval null");
-        }, dur);
-    }
+    this.cancelAnimation = function () {
+      clearInterval(intervalId);
+    };
 
-    rotateAround(target, angleDeg) {
-        const radians = (Math.PI / 180) * angleDeg;
-        const cos = Math.cos(radians);
-        const sin = Math.sin(radians);
-        const nx = (cos * (this.x - target.x)) + (sin * (this.y - target.y)) + target.x;
-        const ny = (cos * (this.y - target.y)) - (sin * (this.x - target.x)) + target.y;
-        this.setX(nx);
-        this.setY(ny);
-    }
+    setTimeout(() => {
+      clearInterval(intervalId);
+    }, dur);
+  }
 
-    moveAround(target, rotationSpeed) {
-        const startDate = performance.now();
-        var lastUpdate = startDate;
-        setInterval(() => {
-            const delta = performance.now() - lastUpdate;
-            lastUpdate = performance.now();
-            this.rotateAround(target, rotationSpeed * delta / 10);
-        }, FPS60)
-    }
+  rotateAround(target, angleDeg) {
+    const radians = (Math.PI / 180) * angleDeg;
+    const cos = Math.cos(radians);
+    const sin = Math.sin(radians);
+    const nx = cos * (this.x - target.x) + sin * (this.y - target.y) + target.x;
+    const ny = cos * (this.y - target.y) - sin * (this.x - target.x) + target.y;
+    this.setX(nx);
+    this.setY(ny);
+  }
 
-    setX(x) {
-        this.x = x;
-        this.el.style.left = `${this.x}vw`;
-    }
-    setY(y) {
-        this.y = y;
-        this.el.style.top = `${this.y}vh`;
-    }
+  moveAround(target, rotationSpeed) {
+    this.cancelAnimation();
+    const startDate = performance.now();
+    var lastUpdate = startDate;
+    intervalId = setInterval(() => {
+      const delta = performance.now() - lastUpdate;
+      lastUpdate = performance.now();
+      this.rotateAround(target, (rotationSpeed * delta) / 10);
+    }, FPS60);
+
+    this.cancelAnimation = function () {
+      clearInterval(intervalId);
+    };
+  }
+
+  setX(x) {
+    this.x = x;
+    this.el.style.left = `${this.x}vw`;
+  }
+  setY(y) {
+    this.y = y;
+    this.el.style.top = `${this.y}vh`;
+  }
 }
---
+++ steps.js
@@ -0,0 +1,122 @@
+var entities = {
+  container: document.getElementById("container"),
+};
+
+var steps = [
+  // Step 1
+  {
+    setup() {
+      entities.cindy = newEntity("button", 50, 50);
+      entities.cindy.el.innerText = "Hey there ! I'm cindy :)";
+    },
+    validate(e) {
+      return true;
+    },
+  },
+  // Step 2
+  {
+    setup() {
+      entities.cindy.moveTo(75, 5, 10000);
+      entities.cindy.el.innerText = "Please stop this...";
+    },
+    validate(e) {
+      return true;
+    },
+  },
+  // Step 3
+  {
+    setup() {
+      entities.cindy.moveTo(5, 5, 1000);
+      entities.cindy.el.innerText = "I said stop...";
+    },
+    validate(e) {
+      return true;
+    },
+  },
+  // Step 4
+  {
+    setup() {
+      entities.textGuardian = newEntity("inputText", 3, 12);
+      entities.textGuardian.el.value = "don't bully my friend !";
+      entities.cindy.el.disabled = true;
+    },
+    validate(e) {
+      if (entities.textGuardian.el.value !== "") {
+        return false;
+      }
+
+      entities.cindy.el.disabled = false;
+
+      if (e.target === entities.cindy.el) {
+        return true;
+      }
+
+      return false;
+    },
+  },
+  // Step 5
+  {
+    setup() {
+      entities.textGuardian.el.disabled = true;
+      entities.cindy.el.disabled = true;
+      entities.cindy.moveTo(59, 8, 600);
+      entities.cindy.el.innerText = "oh checkbox, please help me !";
+
+      entities.cb1 = newEntity("inputCheckbox", 61, 13);
+      entities.cb1.el.checked = true;
+    },
+    validate(e) {
+      if (entities.cb1.checked === true) {
+        return false;
+      }
+      entities.cindy.el.disabled = false;
+
+      if (e.target === entities.cindy.el) {
+        return true;
+      }
+
+      return false;
+    },
+  },
+];
+
+steps[0].setup();
+var currentStep = 0;
+
+function newEntity(type, x, y) {
+  switch (type) {
+    case "button":
+      var el = document.createElement("button");
+      el.addEventListener("click", function (e) {
+        notifyChange(e);
+      });
+      break;
+
+    case "inputText":
+      var el = document.createElement("input");
+      el.type = "text";
+      el.addEventListener("keyup", function (e) {
+        notifyChange(e);
+      });
+      break;
+
+    case "inputCheckbox":
+      var el = document.createElement("input");
+      el.type = "checkbox";
+      el.addEventListener("change", function (e) {
+        notifyChange(e);
+      });
+      break;
+  }
+
+  var entity = new Entity(x, y, el);
+  entities.container.appendChild(entity.el);
+  return entity;
+}
+
+function notifyChange(e) {
+  if (steps[currentStep].validate(e) && currentStep < steps.length - 1) {
+    currentStep++;
+    steps[currentStep].setup();
+  }
+}
